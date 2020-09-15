11240adc961a17aa4ea8d38941bdff6c9e901dd6
Forrest Bicker <bickerforrest@gmail.com>
2020-09-15 00:12:06 -0400

    added macro for fancy button biulding

---
+++ background.js
@@ -0,0 +1,14 @@
+function buildButton(url, icon, section) {
+    document.getElementById(section).innerHTML = 
+        `<div class="column" style="background-color: #FFF; height: 60px; width: 60px; border-radius: 50px">
+            <ul class="icons no-print">
+                <li>
+                    <a target="_blank" href="${url}" class="button button--sacnite" style="border-radius: 50px">
+                        <i class="${icon}" title="${section}"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>`
+}
+
+buildButton("https://github.com/forrestbicker", "fa fa-github", "github-button");buildButton("https://www.linkedin.com/in/forrestbicker/", "fa fa-linkedin-square", "linkedin-button")
