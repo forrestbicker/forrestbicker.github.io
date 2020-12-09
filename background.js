71a4e756f8fa883d93d524642ae2ed1794345ba3
Forrest Bicker <bickerforrest@gmail.com>
2020-12-09 11:49:46 -0500

    move lines and remove extraneous code

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function buildButton(url, icon, section) {
-    document.getElementById(section).innerHTML = 
+    document.getElementById(section).innerHTML =
         `<div class="column" style="background-color: #FFF; height: 60px; width: 60px; border-radius: 50px">
             <ul class="icons no-print">
                 <li>
@@ -11,15 +11,17 @@ function buildButton(url, icon, section) {
         </div>`
 }
 
-buildButton("https://github.com/forrestbicker", "fa fa-github", "github-button");buildButton("https://www.linkedin.com/in/forrestbicker/", "fa fa-linkedin-square", "linkedin-button")
 function buildRandom(n) {
     canvas = document.getElementById("sort");
 
-function buildRandom(width, height, n) {
     let values = []
     min = Math.trunc(0.1 * n)
     for (var i = 0; i < n; i++) {
         values.push(min + i);
     }
-}    maxHeight = canvas.clientHeight / 3 // only display on botom 1/3 of pannel
+    maxHeight = canvas.clientHeight / 3 // only display on botom 1/3 of pannel
     maxWidth = Math.trunc(canvas.clientWidth * 1.1) // make sure extends off of screen
+
+buildButton("https://github.com/forrestbicker", "fa fa-github", "github-button"); buildButton("https://www.linkedin.com/in/forrestbicker/", "fa fa-linkedin-square", "linkedin-button")
+
+buildRandom(80)
