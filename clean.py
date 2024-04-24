e39151491d05f38bebc6aaac162559f7ad1d2a6e
Forrest Bicker <forrestbicker@gmail.com>
2024-04-24 02:53:29 -0700

    revamp blog

---
+++ clean.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+# open every html file in the blog directory and clean the html
+clear_pattern = r'fr-view">(.+)`&lt;clear&gt;`'
+img_pattern = r'<p><span style="font-size: 0.875rem;">`&lt;(.+?)&gt;`</span></p>'
+for file in os.listdir('./blog'):
+    filepath, contents = './blog/' + file, ''
+    with open(filepath, 'r', encoding='utf-8') as f:
+        contents = f.read()
+        new_contents = re.sub(clear_pattern, r'fr-view">', contents)
+        new_contents = re.sub(img_pattern, r'<\g<1>>', new_contents)
+        
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write(new_contents)
+        
+        
+# winning-the-citadel-data-open-championship -> Winning-The-Citadel-Data-Open
+# 
+# Thanks to my teammates Milo Knell, Eric Zhang, Sahil Rane, and Alan Wu, along with our friend and mentor Tiger Che. Shout out to Laurie Luo, Larry Wang, and Luke Wang.
